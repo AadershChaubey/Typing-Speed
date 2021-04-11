17084c2c0c70edb3299ade46b3e98977b255ab70
Aadersh Chaubey <aadersh321chaubey@gmail.com>
2021-04-11 02:32:21 -0700

    added reset button

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const Words = ["Bharat", "Bose", "Chandrashekhar","Atal", "Gopal", "Neelkanthay", "Maharana", "Gandhi", "Vikramaditya", "Tanaji", "Mahadev", "Shastriji", "Shivaji", "Ram", "Krishna", "Suheldev"]
+const Words = ["bharat", "bose", "chandrashekhar","hind", "gopal", "neelkanthay", "maharana", "gandhi", "vikramaditya", "tanaji", "mahadev", "shastriji", "shivaji", "ram", "krishna", "suheldev"]
 var wordSequence = [];
-
+var Timer = null;
 const timer = document.querySelector(".timer")
 const bottom = document.querySelector(".bottom")
 const reportCard = document.querySelector(".report-card")
 var timerStart = false;
 const Input = document.querySelector("input")
 const text = document.querySelector(".text");
-const retryButton = document.querySelector(".retry");
+const retryButton = document.querySelectorAll(".retry");
 var indexOfWordSequence = 0;
 var word = 0;
 var wrongWord = 0;
@@ -50,7 +50,7 @@ Input.addEventListener("keyup", (e)=>{
 
 function fillWords(element){
     let size = Words.length - 1;
-    for(let i = 0; i < 3000; i++){
+    for(let i = 0; i < 1500; i++){
         let newSpan = document.createElement("span");
         let index = (Math.random() * size).toFixed(0);
         newSpan.innerText += Words[index] + " ";
@@ -68,7 +68,7 @@ timer.addEventListener("click", ()=>{
 
 function startTimer(){
     timer.disabled = true;
-    const Timer = setInterval(()=>{
+    Timer = setInterval(()=>{
         let secT = sec.innerText
         let minT = min.innerText;
         secT--;
@@ -78,6 +78,7 @@ function startTimer(){
             if(minT < 0){
                 clearInterval(Timer)
                 timer.style.transform = "scale(0, 0)"
+                retryButton[0].style.transform = "scale(0, 0)"
                 Input.disabled = true;
                 createReportCard()
                 setTimeout(()=>{
@@ -93,12 +94,19 @@ function startTimer(){
     }, 1000)
 }
 
-retryButton.addEventListener("click", ()=>{
+console.log(retryButton)
+retryButton[1].addEventListener("click", ()=>{
     timer.disabled = false;
     reportCard.style.transform = "scale(0, 0)"
     setTimeout(()=>{
         timer.style.transform = "scale(1, 1)"
+        retryButton[0].style.transform = "scale(1, 1)"
     }, 500)
+    resetValues();
+})
+
+retryButton[0].addEventListener("click", ()=>{
+    timer.disabled = false;
     resetValues();
 })
 
@@ -115,6 +123,7 @@ function createReportCard(){
 }
 
 function resetValues(){
+    clearInterval(Timer);
     word = 0;
     wrongWord = 0;
     indexOfWordSequence = 0;
@@ -124,5 +133,6 @@ function resetValues(){
 
     fillWords(text);
     min.innerHTML = time;
+    sec.innerText = 0;
     Input.value = ""
 }
